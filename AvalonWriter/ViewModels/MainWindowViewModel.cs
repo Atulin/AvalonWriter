d95421c4695b222cee490bdbc362d4387431e5b0
Atulin <11233299+Atulin@users.noreply.github.com>
2023-04-17 03:20:24 +0200

    Humble beginnings

--- AvalonWriter/ViewModels/MainWindowViewModel.cs
+++ AvalonWriter/ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,27 @@
-﻿namespace AvalonWriter.ViewModels;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 
-public class MainWindowViewModel : ViewModelBase
+namespace AvalonWriter.ViewModels;
+
+public partial class MainWindowViewModel : ViewModelBase
 {
     public string Greeting => "Welcome to Avalonia!";
+    
+    [ObservableProperty]
+    private int _counter = 0;
+
+    [ObservableProperty]
+    private int _amount = 1;
+
+    [RelayCommand]
+    private void Increment()
+    {
+        Counter += Amount;
+    }
+
+    [RelayCommand]
+    private void Decrement()
+    {
+        Counter -= Amount;
+    }
 }
